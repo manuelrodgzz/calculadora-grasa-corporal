c830e12330ed05de9ff7ad597da3270184f68c55
Manuel Rodriguez <marg.2197@gmail.com>
2022-07-17 12:03:21 -0500

    Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Header from './components/Header'
 import './App.css';
+import './utilities.css';
 
 function App() {
   return (
     <div className="app">
+      <Header />
       <h1>Calculadora de Grasa Corporal</h1>
     </div>
   );
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+
+    return <header className='container'>
+        Health Overview
+    </header>
+}
+
+export default Header
