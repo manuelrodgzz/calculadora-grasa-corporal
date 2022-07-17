a1c9172da0e248dececf0359665da788ef4feffb
Manuel Rodriguez <marg.2197@gmail.com>
2022-07-17 12:06:31 -0500

    Change files casing

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from './components/Header'
-import './App.css';
+import Header from './components/header'
+import './app.css';
 import './utilities.css';
 
 function App() {
